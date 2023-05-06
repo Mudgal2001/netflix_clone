99289569d3fa407161e37eed4fd687c6013404fc
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-05-06 11:06:47 +0530

    firebase created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzqcbhIw8zdnxXghiuZhu7ZdonkGHufGI",
+    authDomain: "netflix-clone-y-8bda1.firebaseapp.com",
+    projectId: "netflix-clone-y-8bda1",
+    storageBucket: "netflix-clone-y-8bda1.appspot.com",
+    messagingSenderId: "483820671709",
+    appId: "1:483820671709:web:17a5c812322307b3440134"
+  };
