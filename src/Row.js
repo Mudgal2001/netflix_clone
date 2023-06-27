669b62f4894d9257d873a95fea308302ebfaf6b7
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-06-27 23:51:10 +0530

    Row under progress

---
+++ src/Row.js
@@ -0,0 +1,34 @@
+import React, { useEffect,useState } from 'react'
+import axios from './axios'
+import "./row.css"
+function Row({title,fetchUrl,isLargeRow = false}) {
+    const [movies,setMovies] = useState([]);
+
+    const baseUrl  = "https://image.tmdb.org/t/p/original/"
+    useEffect(()=>{
+      async function fetchData(){
+        const request = await axios.get(fetchUrl);
+        setMovies(request.data.results);
+        return request;
+      }
+      fetchData();
+    },[fetchUrl])
+    console.log(movies)
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+      
+      {movies.map((movie) =>(
+        <img className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+        key = {movie.id}
+        src={`${baseUrl}${
+            isLargeRow ? movie.poster_path:movie.backdrop_path
+        }`}
+        alt = {movie.name}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Row
